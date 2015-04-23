e3b87b4c1f91409b9b55ce6952e4b41ce43eed19
Andrew Hope <hope.andrew08@gamil.com>
2015-04-23 09:06:49 -0700

    Fix ship movement, bullet movement

--- exercises.js
+++ exercises.js
@@ -11,7 +11,7 @@ Function.prototype.myBind = function(context) {
   var fn = this;
   var args = Array.prototype.slice.call(arguments, 1);
   return function() {
-    fn.apply(context, args)
+    fn.apply(context, args);
   }
 }
 
